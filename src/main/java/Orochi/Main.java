69c0c009868487484f587c6937992785c9650e18
OwOchiii <nguyenvuan2005@gmail.com>
2025-11-13 15:17:45 +0700

    Update Main.java

--- src/main/java/Orochi/Main.java
+++ src/main/java/Orochi/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         WeatherService service = new WeatherService("API_KEY_HERE","localhost",6379);
         try
-        {   //While the args is zipcode, it can work with location name but only in US
+        {   //While the args is zipcode, it can work with location name
             System.out.println("Enter Zipcode or name of Location: ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
